302ca591d14d10d9fd31f410aa576998bce1001f
Hui Kang Tong <username@users.noreply.github.com>
2025-04-18 23:37:31 -0700

    allow multiple swaps in one run

--- algorithms/local_swap.py
+++ algorithms/local_swap.py
@@ -20,73 +20,77 @@ for polling_district, polling_districts in adjacency_data.items():
     for polling_district_other in polling_districts:
         adjacency_pairs.add((polling_district, polling_district_other))
 
-pairs_tried = set()
 
 for iteration in range(3):
+    pairs_tried = set()
     print(f"iteration {iteration}")
     for assignment_idx_1 in range(len(assignments)):
         for assignment_idx_2 in range(assignment_idx_1 + 1, len(assignments)):
             constituency_name_1 = assignments[assignment_idx_1]["constituency_name"]
             constituency_name_2 = assignments[assignment_idx_2]["constituency_name"]
-            polling_districts_1: list[str] = assignments[assignment_idx_1]["polling_districts"]
-            polling_districts_2: list[str] = assignments[assignment_idx_2]["polling_districts"]
-            polling_districts_1_set = set(polling_districts_1)
-            polling_districts_2_set = set(polling_districts_2)
+            for _ in range(10):
+                polling_districts_1: list[str] = assignments[assignment_idx_1]["polling_districts"]
+                polling_districts_2: list[str] = assignments[assignment_idx_2]["polling_districts"]
+                polling_districts_1_set = set(polling_districts_1)
+                polling_districts_2_set = set(polling_districts_2)
 
-            polling_districts_1_for_consideration = set()
-            polling_districts_2_for_consideration = set()
-            for a in polling_districts_1:
-                for b in adjacency_data[a]:
-                    if b in polling_districts_2_set:
-                        polling_districts_1_for_consideration.add(a)
-                        polling_districts_2_for_consideration.add(b)
+                polling_districts_1_for_consideration = set()
+                polling_districts_2_for_consideration = set()
+                for a in polling_districts_1:
+                    for b in adjacency_data[a]:
+                        if b in polling_districts_2_set:
+                            polling_districts_1_for_consideration.add(a)
+                            polling_districts_2_for_consideration.add(b)
 
-            # don't consider all pairs
-            polling_districts_1_for_consideration = list(polling_districts_1_for_consideration)[:: 1 + len(polling_districts_1_for_consideration) // 5]
-            polling_districts_2_for_consideration = list(polling_districts_2_for_consideration)[:: 1 + len(polling_districts_2_for_consideration) // 5]
+                # don't consider all pairs
+                polling_districts_1_for_consideration = list(polling_districts_1_for_consideration)[:: 1 + len(polling_districts_1_for_consideration) // 5]
+                polling_districts_2_for_consideration = list(polling_districts_2_for_consideration)[:: 1 + len(polling_districts_2_for_consideration) // 5]
 
-            escape = False
-            for a in polling_districts_1_for_consideration:
-                if escape:
-                    break
-                polling_districts_1.remove(a)
-                polling_districts_2.append(a)
-                validated, _ = validate_assignment(assignment_data)
-                if validated:
-                    score = score_assignment(assignment_data)["overall_score"]
-                    if score > best_score:
-                        print(f"{score} after moving {a} from {constituency_name_1} to {constituency_name_2}")
-                        best_score = score
-                        save_json(assignment_data, "assignments/local_swap.json")
-                        escape = True
-                        continue
-                polling_districts_1.append(a)
-                polling_districts_2.remove(a)
-
-            for a in polling_districts_1_for_consideration:
-                if escape:
-                    break
-                for b in polling_districts_2_for_consideration:
-                    if escape:
+                improvement_found = False
+                for a in polling_districts_1_for_consideration:
+                    if improvement_found:
                         break
-                    if (a, b) in pairs_tried:
-                        continue
-                    pairs_tried.add((a, b))
-                    pairs_tried.add((b, a))
                     polling_districts_1.remove(a)
-                    polling_districts_1.append(b)
-                    polling_districts_2.remove(b)
                     polling_districts_2.append(a)
                     validated, _ = validate_assignment(assignment_data)
                     if validated:
                         score = score_assignment(assignment_data)["overall_score"]
                         if score > best_score:
-                            print(f"{score} after swapping {a} from {constituency_name_1} with {b} from {constituency_name_2}")
+                            print(f"{score} after moving {a} from {constituency_name_1} to {constituency_name_2}")
                             best_score = score
                             save_json(assignment_data, "assignments/local_swap.json")
-                            escape = True
+                            improvement_found = True
                             continue
-                    polling_districts_1.remove(b)
                     polling_districts_1.append(a)
                     polling_districts_2.remove(a)
-                    polling_districts_2.append(b)
+
+                for a in polling_districts_1_for_consideration:
+                    if improvement_found:
+                        break
+                    for b in polling_districts_2_for_consideration:
+                        if improvement_found:
+                            break
+                        if (a, b) in pairs_tried:
+                            continue
+                        pairs_tried.add((a, b))
+                        pairs_tried.add((b, a))
+                        polling_districts_1.remove(a)
+                        polling_districts_1.append(b)
+                        polling_districts_2.remove(b)
+                        polling_districts_2.append(a)
+                        validated, _ = validate_assignment(assignment_data)
+                        if validated:
+                            score = score_assignment(assignment_data)["overall_score"]
+                            if score > best_score:
+                                print(f"{score} after swapping {a} from {constituency_name_1} with {b} from {constituency_name_2}")
+                                best_score = score
+                                save_json(assignment_data, "assignments/local_swap.json")
+                                improvement_found = True
+                                continue
+                        polling_districts_1.remove(b)
+                        polling_districts_1.append(a)
+                        polling_districts_2.remove(a)
+                        polling_districts_2.append(b)
+
+                if not improvement_found:
+                    break
